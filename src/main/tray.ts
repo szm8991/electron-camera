ab8dd640f391c2ad4feb9890bd1964adc197d79d
songzheming <songzheming@163.com>
2023-07-20 13:54:48 +0800

    update: add builder config and install icon

--- src/main/tray.ts
+++ src/main/tray.ts
@@ -11,7 +11,7 @@ const createTray = () => {
   )
   const contextMenu = Menu.buildFromTemplate([
     { label: '退出', role: 'quit' },
-    { label: '参考', click: () => shell.openExternal('https://www.houdunren.com') }
+    { label: '来源', click: () => shell.openExternal('https://www.houdunren.com') }
   ])
   tray.setToolTip('electron摄像头')
   tray.setContextMenu(contextMenu)
